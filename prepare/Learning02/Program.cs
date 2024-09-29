8c71c4d8789f9eb8163b7f5bbdba62ee789801b3
Noble-smart <nonwuemeka@gmail.com>
2024-09-29 12:52:10 +0100

    Week 2 Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
+// Program.cs
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create jobs
+        Job job1 = new Job();
+        job1.Company = "Microsoft";
+        job1.JobTitle = "Software Engineer";
+        job1.StartYear = 2019;
+        job1.EndYear = 2022;
+
+        Job job2 = new Job();
+        job2.Company = "Apple";
+        job2.JobTitle = "Manager";
+        job2.StartYear = 2022;
+        job2.EndYear = 2023;
+
+        // Create a new resume
+        Resume myResume = new Resume();
+        myResume.Name = "Allison Rose";
+
+        // Add jobs to the resume
+        myResume.Jobs.Add(job1);
+        myResume.Jobs.Add(job2);
+
+        // Display resume
+        myResume.DisplayResume();
+
+        Console.ReadKey(); // Pause the program before closing
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,24 @@
+// Resume.cs
+
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string Name { get; set; }
+    public List<Job> Jobs { get; set; }
+
+    public Resume()
+    {
+        Jobs = new List<Job>();
+    }
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine("Jobs:");
+        foreach (var job in Jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
