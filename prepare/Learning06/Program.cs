87e00a09bbeb2ef9cf16a71324e029c35aec2811
Noble-smart <nonwuemeka@gmail.com>
2024-10-31 00:15:20 +0100

    Week 5 learning Activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Create shapes
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        // Create list of shapes
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle
+        };
+
+        // Iterate through list and display shape information
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.Color}, Area: {shape.GetArea():F2}");
+        }
+
+        Console.ReadKey();
     }
 }
